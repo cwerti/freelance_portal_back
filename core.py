d1e79524a8364f89413233f509f1d0b876abe483
holaqz <arturhajtbaev2260@gmail.com>
2025-04-25 20:12:06 +0500

    DB models

---
+++ core.py
@@ -0,0 +1,28 @@
+"""
+Стартовая настройка для sqlalchemy
+"""
+import datetime
+from collections.abc import Callable
+
+from sqlalchemy import MetaData, func, Column, DateTime
+from sqlalchemy.orm import DeclarativeMeta, declarative_base
+
+Base: DeclarativeMeta = declarative_base()
+metadata: MetaData = Base.metadata
+metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+def fresh_timestamp() -> Callable[[], datetime.datetime]:
+    return func.timezone("UTC", func.now())
+
+
+class TimestampMixin:
+    created_at = Column(DateTime, default=fresh_timestamp())
+    updated_at = Column(DateTime, default=fresh_timestamp(), onupdate=fresh_timestamp())
+    deleted_at = Column(DateTime)
